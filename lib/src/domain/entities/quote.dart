8788db50d178d659f697bd63e655de3881c6a484
Karan <karanjain2527@gmail.com>
2026-03-20 12:54:49 +0530

    screenshots added

--- lib/src/domain/entities/quote.dart
+++ lib/src/domain/entities/quote.dart
@@ -1,3 +1,4 @@
+//All objects of this class are immutable, so we can use const constructor
 class Quote {
   const Quote({required this.id, required this.content, required this.author});
 
--- lib/src/domain/repositories/quote_repository.dart
+++ lib/src/domain/repositories/quote_repository.dart
@@ -1,3 +1,4 @@
+//This file defines the QuoteRepository interface, which declares a method for fetching a random quote.
 import '../entities/quote.dart';
 
 abstract class QuoteRepository {
